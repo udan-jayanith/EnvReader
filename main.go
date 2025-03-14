5d08a07b379ceac4308dbd8cc027473029f9d0ca
Udan Jayanith Jayakody <j.udanjayanith@gmail.com>
2025-03-14 20:06:03 +0530

    update main.go and main_test.go

--- main.go
+++ main.go
@@ -1,3 +1,6 @@
+// .env file reader for go.
+//
+// `envReader "github.com/udan-jayanith/envReader"`
 package envReader
 
 import (
@@ -20,8 +23,8 @@ func (env *Env) setKeyValue(key, value string) {
 
 var EnvFile Env
 
-//LoadEnv reads the .env file and stores it in the Env struct.
-func LoadEnv(){
+// LoadEnv reads the .env file and stores it in the Env struct.
+func LoadEnv() {
 	file, err := os.Open(".env")
 	if err != nil {
 		fmt.Println(err)
@@ -32,7 +35,7 @@ func LoadEnv(){
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		envFileLine := scanner.Bytes()
-		key, value := "", ""
+		key, value := []byte{}, []byte{}
 		isKey := true
 		for _, v := range envFileLine {
 			// 32 means space. A tab is equal 4 or 3 spaces(32)
@@ -46,20 +49,20 @@ func LoadEnv(){
 					isKey = false
 					continue
 				}
-				key += string(v)
+				key = append(key, v)
 			} else {
 				// 34 means "
 				if v == 34 {
 					continue
 				}
-				value += string(v)
+				value = append(value, v)
 			}
 		}
 
-		if key == "" || key == " " {
+		if key == nil || string(key) == "" || string(key) == " " {
 			continue
 		}
-		EnvFile.setKeyValue(key, value)
+		EnvFile.setKeyValue(string(key), string(value))
 	}
 
 	err = scanner.Err()
@@ -75,8 +78,26 @@ func (env *Env) get(key string) string {
 	return env.envMap[key]
 }
 
-// Get returns the value for a given key.  
-// If the .env file hasn't been read yet, Get will read the .env file and then return the value for the given key.  
-func Get(key string) string{
+// Get returns the value for a given key.
+//
+// If the .env file hasn't been read yet, Get will read the .env file and then return the value for the given key.
+func Get(key string) string {
 	return EnvFile.get(key)
+}
+
+// GetEnvMap returns a map of the .env file.
+func GetEnvMap() map[string]string {
+	if EnvFile.envMap == nil {
+		LoadEnv()
+	}
+	return EnvFile.envMap
+}
+
+// SetEnvVars sets environment variables using os.Setenv() for each key-value pair in EnvStruct.
+//
+// This allows the environment variables to be retrieved using os.Getenv().
+func SetEnvVars() {
+	for k, v := range GetEnvMap() {
+		os.Setenv(k, v)
+	}
 }
--- main_test.go
+++ main_test.go
@@ -2,6 +2,7 @@ package envReader_test
 
 import (
 	"testing"
+	"os"
 
 	envReader "github.com/udan-jayanith/envReader"
 )
@@ -15,5 +16,18 @@ func TestGet(t *testing.T) {
 	t.Logf("\n %s \n", port)
 	if port != ":8080"{
 		t.Error("Unexpected value at TestGet ", port)
+	}
+}
+
+func TestGetEnvMap(t *testing.T){
+	envMap := envReader.GetEnvMap()
+	t.Logf("\n %s \n", envMap)
+}
+
+func TestSetEnvVars(t *testing.T){
+	envReader.SetEnvVars()
+
+	if os.Getenv("Port") != ":8080" {
+		t.Error("Unexpected return value at TestSetEnvVars")
 	}
 }
